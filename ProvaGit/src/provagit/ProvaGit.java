c2356ebef3c2d12f72935125e58fa853817f58f1
manduca.fabio <manduca.fabio@TAVE-PC18.OSDBSESTO.local>
2020-11-17 13:10:11 +0100

    add bello bellissimo!

---
+++ ProvaGit/src/provagit/ProvaGit.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provagit;
+
+/**
+ *
+ * @author manduca.fabio
+ */
+public class ProvaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i=1;
+        bello bellissimo = new bello();
+        while(true){
+        
+            System.out.println("Quanti motorini hai scavallato? "+i+""+bellissimo.bellissimo ); 
+            i++;
+        }
+    }
+    
+}
